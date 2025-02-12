22c65307a144c9d03a381051eb74053731cbb020
jsk0910 <sangkyun.jeon@gmail.com>
2025-02-12 18:13:35 +0900

    update Router

--- research_lab/app/Config/Routes.php
+++ research_lab/app/Config/Routes.php
@@ -12,3 +12,4 @@ $routes->get('/members/jeongilseo', 'MembersController::jeongilseo');
 $routes->get('members/(:segment)', 'MembersController::member_details/$1');
 $routes->get('/projects', 'Projects::index');
 $routes->get('/publication', 'Publications::index');
+$routes->get('/pdf/view/cv/(:segment)', 'PdfController::show_pdf/$1');
